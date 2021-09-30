7e22d85ca824e2e1a467ed85366466fc4be46e08
Toshpolat <okmkhk@gmail.com>
2021-09-30 16:44:57 +0500

    8.1

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,7 +1,9 @@
 module.exports = {
-    MONGODB_URI: "mongodb+srv://texnar1225:texnar1225@cluster0.ov7ap.mongodb.net/Pharmacy",
-    SESSION_SECRET: "some secret value",
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'nodejs@email.ru',
-    BASE_URL: 'http://localhost:3000'
-}
+        MONGODB_URI: "mongodb+srv://texnar1225:texnar1225@cluster0.ov7ap.mongodb.net/Pharmacy",
+        SESSION_SECRET: "some secret value",
+        SENDGRID_API_KEY: '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',
+        EMAIL_FROM: 'nodejs@email.ru',
+        BASE_URL: 'http://localhost:3000'
+    }
+    /* https://git.heroku.com/pharmaclick777.git
+     https://pharmaclick777.herokuapp.com/ */
