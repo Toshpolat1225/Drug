57a557616d02dcaafd9bb04ee5e703760acc8da1
Toshpolat <okmkhk@gmail.com>
2021-09-30 15:48:49 +0500

    Signed-off-by: Toshpolat <okmkhk@gmail.com>

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-    MONGODB_URI: " mongodb+srv://texnar1225:texnar1225@cluster0.ov7ap.mongodb.net/Pharmacy",
+    MONGODB_URI: "mongodb+srv://texnar1225:texnar1225@cluster0.ov7ap.mongodb.net/Pharmacy",
     SESSION_SECRET: "some secret value",
     SENDGRID_API_KEY: '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',
     EMAIL_FROM: 'nodejs@email.ru',
