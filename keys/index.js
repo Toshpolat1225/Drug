3dc65390277443e870ef7452cc60b48adbf5ec37
Toshpolat <okmkhk@gmail.com>
2021-09-30 14:25:10 +0500

    8.3

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-    MONGODB_URI: " mongodb+srv://texnar1225:texnar1225@cluster0.ov7ap.mongodb.net/Pharmacy",
-    SESSION_SECRET: "some secret value",
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'nodejs@email.ru',
-    BASE_URL: 'http://localhost:3000'
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.dev')
 }
